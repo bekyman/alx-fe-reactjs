bbd63714de78b0ac5915df8f1f41165c3a3b26f9
bekyman <51075542+bekyman@users.noreply.github.com>
2026-01-13 21:57:34 +0300

    Refactor Header component styles and structure

--- alx-react-app-new/src/Header.jsx
+++ alx-react-app-new/src/Header.jsx
@@ -1,7 +1,23 @@
-function Header(){
+function Header() {
   return (
-    <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
-     <h1>My Favorite Cities</h1>
+    <header
+      style={{
+        border: '1px solid gray',
+        padding: '10px',
+        margin: '10px',
+        backgroundColor: 'navy',
+        textAlign: 'center'
+      }}
+    >
+      <h1
+        style={{
+          color: 'white',
+          fontWeight: 'bold',
+          margin: 0
+        }}
+      >
+        My Favorite Cities
+      </h1>
     </header>
   );
 }
