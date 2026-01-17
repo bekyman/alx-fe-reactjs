0c43ddb5edc1a149aabe562821ea1abd10857a44
bekyman <51075542+bekyman@users.noreply.github.com>
2026-01-17 10:32:11 +0300

    Update UserContext.js

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,17 +1,6 @@
-import React, { useContext } from 'react';
-import UserContext from './UserContext'; // Adjust path if UserContext is in a different folder
+import React, { createContext } from 'react';
 
-const UserProfile = () => {
-  const data = useContext(UserContext); // Consuming the context
+// Initialize the Context
+const UserContext = createContext();
 
-  return (
-    <div>
-      <h2>User Profile</h2>
-      {/* Render data safely checking if it exists */}
-      <p>Name: {data ? data.name : 'Loading...'}</p>
-      <p>Email: {data ? data.email : 'Loading...'}</p>
-    </div>
-  );
-};
-
-export default UserProfile;
+export default UserContext;
