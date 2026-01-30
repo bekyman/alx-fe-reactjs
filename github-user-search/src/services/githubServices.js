adad6e3c1d472ad7853fc3949b9d306b07cc3292
bekyman <51075542+bekyman@users.noreply.github.com>
2026-01-30 22:56:40 +0300

    Add fetchUserData function to retrieve user data

--- github-user-search/src/services/githubServices.js
+++ github-user-search/src/services/githubServices.js
@@ -1,1 +1,7 @@
+import axios from 'axios';
 
+// Function to fetch a single user's data
+export const fetchUserData = async (username) => {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    return response.data;
+};
